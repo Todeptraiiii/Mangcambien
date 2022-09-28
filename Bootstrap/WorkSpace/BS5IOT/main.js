8f55b34c78a264407e4317a5c9b80a534540bed0
daovanto <daovanto@gmail.com>
2022-09-28 15:07:56 +0700

    HighchartVer1

--- Bootstrap/WorkSpace/BS5IOT/main.js
+++ Bootstrap/WorkSpace/BS5IOT/main.js
@@ -1,79 +1,96 @@
-Highcharts.chart('container', {
-
+// import { arrayTime } from "./main2.js";
+const chart = Highcharts.chart("container", {
   title: {
-    text: 'U.S Solar Employment Growth by Job Category, 2010-2020'
+    // text: "U.S Solar Employment Growth by Job Category, 2010-2020",
+    text: "Realtime Temperature,Humadity,Light",
   },
 
   subtitle: {
-    text: 'Source: <a href="https://irecusa.org/programs/solar-jobs-census/" target="_blank">IREC</a>'
+    text: 'Source: <a href="https://irecusa.org/programs/solar-jobs-census/" target="_blank">IREC</a>',
   },
 
   yAxis: {
     title: {
-      text: 'Number of Employees'
-    }
+      text: "Numer of Data",
+    },
   },
 
-  // xAxis: {
-  //   accessibility: {
-  //     rangeDescription: 'Range: 2010 to 2020'
-  //   }
-  // },
   xAxis: {
-    labels: {
-      format: '{value} km'
-    },
-    minRange: 1,
-    title: {
-      text: 'Distance'
-    },
+    // labels: {
+    //   enabled: true,
+    //   formatter: function(){ return arrayTime[this.value]}
+    // },
     accessibility: {
-      rangeDescription: 'Range: 0 to 187.8km.'
-    }
+      rangeDescription: "Range: 2010 to 2020",
+    },
   },
 
   legend: {
-    layout: 'vertical',
-    align: 'right',
-    verticalAlign: 'middle'
+    layout: "vertical",
+    align: "right",
+    verticalAlign: "middle",
   },
 
   plotOptions: {
     series: {
       label: {
-        connectorAllowed: false
+        connectorAllowed: false,
       },
-      pointStart: 2010
-    }
+      // pointStart: 1,
+    },
   },
 
-  series: [{
-    name: 'Nhiệt độ',
-    data: [43934, 48656, 65165, 81827, 112143, 142383,
-      171533, 165174, 155157, 161454, 154610]
-  }, {
-    name: 'Độ ẩm',
-    data: [24916, 37941, 29742, 29851, 32490, 30282,
-      38121, 36885, 33726, 34243, 31050]
-  }, {
-    name: 'Ánh sáng',
-    data: [11744, 30000, 16005, 19771, 20185, 24377,
-      32147, 30912, 29243, 29213, 25663]
-  },],
+  series: [
+    {
+      name: "Nhiệt Độ",
+      // data: [
+      //   43, 48, 65, 81, 11, 14, 17, 16, 15,
+      //   16, 15,
+      // ],
+      // data: arrayN
+    },
+    {
+      name: "Độ ẩm",
+      // data: [24, 37, 29, 29, 32, 30,
+      //   38, 36, 33, 34, 31]
+      // data: arrayN,
+    },
+    {
+      name: "Ánh sáng",
+      // data: [
+      //   11, 30, 16, 19, 20, 24, 32, 30, 29, 29,
+      //   25,
+      // ],
+    },
+  ],
 
   responsive: {
-    rules: [{
-      condition: {
-        maxWidth: 500
+    rules: [
+      {
+        condition: {
+          maxWidth: 500,
+        },
+        chartOptions: {
+          legend: {
+            layout: "horizontal",
+            align: "center",
+            verticalAlign: "bottom",
+          },
+        },
       },
-      chartOptions: {
-        legend: {
-          layout: 'horizontal',
-          align: 'center',
-          verticalAlign: 'bottom'
-        }
-      }
-    }]
-  }
-
+    ],
+  },
 });
+
+export function redrawChart(dataT,dataH,dataL){
+  chart.series[0].setData(dataT);
+  chart.series[1].setData(dataH);
+  chart.series[2].setData(dataL);
+  // console.log();
+}
+// export function redrawChart0(data){
+//   chart.series[0].setData(data);
+// }
+// export function redrawChart2(data){
+//   chart.series[2].setData(data);
+// }
--- Bootstrap/WorkSpace/BS5IOT/main2.js
+++ Bootstrap/WorkSpace/BS5IOT/main2.js
@@ -1,95 +1,125 @@
-const buttonLed1ON =  document.querySelector('#led1on');
-const buttonLed1OFF = document.querySelector('#led1off');
-const buttonLed2ON  = document.querySelector('#led2on');
-const buttonLed2OFF = document.querySelector('#led2off');
-const bgled1        = document.querySelector('#bgled1');
-const bgled2        = document.querySelector('#bgled2');
-let arrayN = [];
+import { redrawChart } from "./main.js";
+
+export const arrayT =[];
+export const arrayH =[];
+export const arrayL =[];
+// export const arrayTime = [];
+const buttonLed1ON = document.querySelector("#led1on");
+const buttonLed1OFF = document.querySelector("#led1off");
+const buttonLed2ON = document.querySelector("#led2on");
+const buttonLed2OFF = document.querySelector("#led2off");
+const bgled1 = document.querySelector("#bgled1");
+const bgled2 = document.querySelector("#bgled2");
 // console.log(buttonLed2OFF);
 // console.log(bgled1);
 // console.log(buttonLed1OFF);
 // console.log(buttonLed1ON);
 // console.log(buttonLed1ON.style);
-buttonLed1ON.onclick = function(){
-    const a = confirm('Xác nhận bật');
-    if(a== true){
-    buttonLed1ON.style.backgroundColor = '#1565c0';
-    buttonLed1ON.style.color = 'white';
-    buttonLed1OFF.style.backgroundColor = 'white';
-    buttonLed1OFF.style.color = 'black';
-    bgled1.style.backgroundColor = '#f7797d';
-    }
-}
-buttonLed1OFF.onclick = function(){
-    const a = confirm('Xác nhận tắt');
-    if(a == true){
-    buttonLed1OFF.style.backgroundColor = '#1565c0';
-    buttonLed1OFF.style.color = 'white';
-    buttonLed1ON.style.backgroundColor = 'white';
-    buttonLed1ON.style.color = 'black';
-    bgled1.style.backgroundColor = 'white';
-    }
-}
-buttonLed2ON.onclick = function(){
-    const a = confirm('Xác nhận bật');
-    if(a == true){
-        // buttonLed2ON.style.backgroundColor = '#1565c0';
-        // buttonLed2ON.style.color = 'white';
-        // buttonLed2OFF.style.backgroundColor = 'white';
-        // buttonLed2OFF.style.color = 'black';
-        // bgled2.style.backgroundColor = 'red';
-        document.getElementById("myImage").src = "./Images/bongdensang.jpg";
-    }
-}
-buttonLed2OFF.onclick = function(){
-    const a = confirm('Xác nhận tắt');
-    if(a == true){
-        // buttonLed2OFF.style.backgroundColor = '#1565c0';
-        // buttonLed2OFF.style.color = 'white';
-        // buttonLed2ON.style.backgroundColor = 'white';
-        // buttonLed2ON.style.color = 'black';
-        // bgled2.style.backgroundColor = 'white';
-        document.getElementById("myImage").src = "./Images/bongden.jpg";
-    }
-}
-function timechange(){
-// // thay đổi nhiệt độ
-const nhietdo = document.getElementById('nhietdo');
-const changeN1 = Math.floor(Math.floor(Math.random()*45));
-arrayN.push(changeN1);
-console.log(arrayN);
+// function getTime(){
+//     const date = new Date();
+//     return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+// }
+buttonLed1ON.onclick = function () {
+  const a = confirm("Xác nhận bật");
+  if (a == true) {
+    buttonLed1ON.style.backgroundColor = "#1565c0";
+    buttonLed1ON.style.color = "white";
+    buttonLed1OFF.style.backgroundColor = "white";
+    buttonLed1OFF.style.color = "black";
+    bgled1.style.backgroundColor = "#f7797d";
+  }
+};
+buttonLed1OFF.onclick = function () {
+  const a = confirm("Xác nhận tắt");
+  if (a == true) {
+    buttonLed1OFF.style.backgroundColor = "#1565c0";
+    buttonLed1OFF.style.color = "white";
+    buttonLed1ON.style.backgroundColor = "white";
+    buttonLed1ON.style.color = "black";
+    bgled1.style.backgroundColor = "white";
+  }
+};
+buttonLed2ON.onclick = function () {
+  const a = confirm("Xác nhận bật");
+  if (a == true) {
+    buttonLed2ON.style.backgroundColor = '#1565c0';
+    buttonLed2ON.style.color = 'white';
+    buttonLed2OFF.style.backgroundColor = 'white';
+    buttonLed2OFF.style.color = 'black';
+    document.getElementById("myImage").src = "./Images/bongdensang.jpg";
+  }
+};
+buttonLed2OFF.onclick = function () {
+  const a = confirm("Xác nhận tắt");
+  if (a == true) {
+    buttonLed2OFF.style.backgroundColor = '#1565c0';
+    buttonLed2OFF.style.color = 'white';
+    buttonLed2ON.style.backgroundColor = 'white';
+    buttonLed2ON.style.color = 'black';
+    document.getElementById("myImage").src = "./Images/bongden.jpg";
+  }
+};
+function timechange() {
+  // // thay đổi nhiệt độ
+  const nhietdo = document.getElementById("nhietdo");
+  const changeN1 = Math.floor(Math.floor(Math.random() * 45));
+  if (arrayT.length <= 10) {
+    arrayT.push(changeN1);
+  } else {
+    arrayT.splice(0, 1);
+    arrayT.push(changeN1);
+  }
+//   console.log(arrayT);
 // console.log(nhietdo.innerText);
-const changeN  = nhietdo.innerText = `${changeN1} \xB0C` ;
-if(changeN1 >= 33){
-    document.getElementById('cotNhietdo').style.backgroundColor  = 'red';
-}
-else{
-    document.getElementById('cotNhietdo').style.backgroundColor = 'gray';
-}
-// thay đổi độ ẩm
-const doam = document.getElementById('doam');
-const changeD1 = Math.floor(Math.floor(Math.random()*100));
-const changeD = doam.innerText = changeD1 + '%';
+  const changeN = (nhietdo.innerText = `${changeN1} \xB0C`);
+  if (changeN1 >= 33) {
+    document.getElementById("cotNhietdo").style.backgroundColor = "red";
+  } else {
+    document.getElementById("cotNhietdo").style.backgroundColor = "gray";
+  }
+  // thay đổi độ ẩm
+  const doam = document.getElementById("doam");
+  const changeD1 = Math.floor(Math.floor(Math.random() * 100));
+  const changeD = (doam.innerText = changeD1 + "%");
+  if(arrayH.length <= 10) {
+    arrayH.push(changeD1);
+  }else{
+    arrayH.splice(0,1);
+    arrayH.push(changeD1);
+  }
 
-// console.log(changeD.length);
+  // console.log(changeD.length);
 
-if(changeD1 >= 40){
-    document.getElementById('cotDoam').style.backgroundColor = 'green';
-}
-else{
-    document.getElementById('cotDoam').style.backgroundColor = 'orange';
-}
-// thay đổi ánh sáng
+  if (changeD1 >= 40) {
+    document.getElementById("cotDoam").style.backgroundColor = "green";
+  } else {
+    document.getElementById("cotDoam").style.backgroundColor = "orange";
+  }
+  // thay đổi ánh sáng
 
-const anhsang = document.getElementById('anhsang');
-// console.log(anhsang);
-const changeA  = Math.floor(Math.random()*188);
-anhsang.innerText =`${changeA} lux`
-if(changeA >= 50){
-    document.getElementById('cotAnhsang').style.backgroundColor = 'blue';
+  const anhsang = document.getElementById("anhsang");
+  // console.log(anhsang);
+  const changeA = Math.floor(Math.random() * 188);
+  anhsang.innerText = `${changeA} lux`;
+  if (changeA >= 50) {
+    document.getElementById("cotAnhsang").style.backgroundColor = "blue";
+  } else {
+    document.getElementById("cotAnhsang").style.backgroundColor = "purple";
+  }
+  if(arrayL.length <= 10){
+    arrayL.push(changeA);
+  }else{
+    arrayL.splice(0,1);
+    arrayL.push(changeA);
+  }
+  // if(arrayTime.length <= 10){
+  //   arrayTime.push(getTime());
+  // }
+  // else{
+  //   arrayTime.splice(0,1);
+  //   arrayTime.push(getTime());
+  // }
+  redrawChart(arrayT,arrayH,arrayL);
 }
-else{
-    document.getElementById('cotAnhsang').style.backgroundColor = 'purple';
-}
-}
-setInterval(timechange,5000);
+setInterval(timechange, 2000);
+
